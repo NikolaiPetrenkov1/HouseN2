ab6eea930fa201f564f2749e3c21af0fa8358220
Петренков Николай <@corp.itstep>
2017-12-29 11:31:59 +0600

    MyHouse

---
+++ Ipart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHouse
+{
+    public interface IPart
+    {
+        bool IsBuild { get; }
+
+        void BuildPart();
+
+        string Show();
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHouse
+{
+    public interface IWorker
+    {
+        bool IsWorkFinish { get; }
+        void Work(House house);
+    }
+}
